5a1ae2ae0e9531566d01c99e1120d5da346454d3
Robzm <172452133+Robzm@users.noreply.github.com>
2025-02-28 16:47:33 -0500

    chore

--- src/services/api.js
+++ src/services/api.js
@@ -45,8 +45,7 @@ export default {
       throw error;
     }
   },
-  // Método para obtener una lista de videos (para el grid)
-  async getVideoList(channelId, apiKey, maxResults = 9) {
+  async getVideoList(channelId, apiKey, maxResults = 9, pageToken = null) {
     try {
       const response = await apiClient.get('/search', {
         params: {
@@ -56,9 +55,10 @@ export default {
           order: 'date',
           type: 'video',
           key: apiKey,
+          pageToken: pageToken, // Usa el parámetro pageToken
         },
       });
-      return response.data.items; // Retorna la lista de videos
+      return response.data; // Devuelve toda la respuesta (incluyendo nextPageToken)
     } catch (error) {
       console.error('Error fetching video list:', error);
       throw error;
@@ -78,6 +78,23 @@ export default {
       return response.data.items[0]; // Retorna los detalles del video
     } catch (error) {
       console.error('Error fetching video details:', error);
+      throw error;
+    }
+  },
+
+  async getPlaylists(channelId, apiKey, maxResults = 10) {
+    try {
+      const response = await apiClient.get('/playlists', {
+        params: {
+          part: 'snippet',
+          channelId: channelId,
+          maxResults: maxResults,
+          key: apiKey,
+        },
+      });
+      return response.data.items;
+    } catch (error) {
+      console.error('Error fetching playlists:', error);
       throw error;
     }
   },
